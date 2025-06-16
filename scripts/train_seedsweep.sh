d5d374a52eb2131a43de7cd204462acd2a450b24
tgmorton <thomasmorton88@gmail.com>
2025-06-16 13:11:39 -0700

    fix: update partition and config file for seed sweep script

--- scripts/train_seedsweep.sh
+++ scripts/train_seedsweep.sh
@@ -5,6 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --time=7-0:00:00                   # Time limit (D-HH:MM:SS)
 #SBATCH --output=../logs/%x-%A_%a.out      # Unique standard output log for each array task
